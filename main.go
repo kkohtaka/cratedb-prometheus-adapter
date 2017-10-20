803a69b2b693e1f4544a5f7004e8f1ae0b1efc35
Kazumasa Kohtaka <kkohtaka@gmail.com>
2017-10-20 23:11:55 +0900

    Add github.com/crate/crate_adapter and its dependees as vendored packages

---
+++ main.go
@@ -0,0 +1,8 @@
+// Copyright (C) 2017 Kazumasa Kohtaka <kkohtaka@gmail.com> All right reserved
+// This file is available under the MIT license.
+
+package main
+
+import _ "github.com/crate/crate_adapter"
+
+func main() {}
